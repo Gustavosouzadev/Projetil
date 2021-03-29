672a7fe1627be1f828ad63534bb3116d6f845bf0
Gustavosouzadev <gustavoduti2003@gmail.com>
2021-03-28 23:12:44 -0300

    Teste

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Projetil
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
